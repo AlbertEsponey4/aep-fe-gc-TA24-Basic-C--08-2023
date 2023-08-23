ef63822d0cfa5001d934406b7ee48d77e50a671e
AlbertEsponey4 <126797937+AlbertEsponey4@users.noreply.github.com>
2023-08-23 17:55:35 +0200

    m3 i m4 acabats

--- Program.cs
+++ Program.cs
@@ -313,19 +313,286 @@ namespace Tarea24
         }
     }
 
-    class M3 
-    { 
+    class M3
+    {
         public static void Execute()
         {
+            /***************************/
+            /****                   ****/
+            /****    MILESTONE 1    ****/
+            /****                   ****/
+            /***************************/
 
+            Console.WriteLine("MILESTONE 1: Noms Ciutats\n");
+
+            /********* FASE 1 **********/
+
+            string ciutat1, ciutat2, ciutat3, ciutat4, ciutat5, ciutat6;
+
+            Console.Write("Introdueix el nom de la ciutat 1: ");
+            ciutat1 = Console.ReadLine();
+            Console.Write("Introdueix el nom de la ciutat 2: ");
+            ciutat2 = Console.ReadLine();
+            Console.Write("Introdueix el nom de la ciutat 3: ");
+            ciutat3 = Console.ReadLine();
+            Console.Write("Introdueix el nom de la ciutat 4: ");
+            ciutat4 = Console.ReadLine();
+            Console.Write("Introdueix el nom de la ciutat 5: ");
+            ciutat5 = Console.ReadLine();
+            Console.Write("Introdueix el nom de la ciutat 6: ");
+            ciutat6 = Console.ReadLine();
+
+            Console.WriteLine($"{ciutat1}, {ciutat2}, {ciutat3}, {ciutat4}, {ciutat5}, {ciutat6}");
+
+            /********* FASE 2 **********/
+
+            string[] arrayCiutats = { ciutat1, ciutat2, ciutat3, ciutat4, ciutat5, ciutat6 };
+
+            //ordena les ciutats alfabeticament
+            Array.Sort(arrayCiutats);
+
+            Console.WriteLine("Noms de ciutats ordenades per ordre alfabètic:");
+            foreach (string ciutat in arrayCiutats)
+            {
+                Console.WriteLine(ciutat);
+            }
+
+            /********* FASE 3 **********/
+
+            string[] arrayCiutatsModificades = new string[arrayCiutats.Length];
+            for (int i = 0; i < arrayCiutats.Length; i++)
+            {
+                //Replace canvia tots els caracters del string, en aquest cas 'a' per el caracter del segon parametre del a funcio
+                arrayCiutatsModificades[i] = arrayCiutats[i].Replace('a', '4');
+            }
+
+            Console.WriteLine("Noms de ciutats modificades:");
+            foreach (string ciutatModificada in arrayCiutatsModificades)
+            {
+                Console.WriteLine(ciutatModificada);
+            }
+
+            /********* FASE 4 **********/
+
+            //array buit del tamany de arrayCiutats
+            string[] arrayNomsInvertits = new string[arrayCiutats.Length];
+
+            for (int i = 0; i < arrayCiutats.Length; i++)
+            {
+                //per a cada ciutat transformo l'string en chars i els poso al array de char
+                char[] ciutatChars = arrayCiutats[i].ToCharArray();
+                //inverteixo
+                Array.Reverse(ciutatChars);
+                //afegeixo la ciutat invertida ala rray buit de ciutats ingvertides
+                arrayNomsInvertits[i] = new string(ciutatChars);
+            }
+
+            //les mostro
+            Console.WriteLine("Noms de ciutats invertits:");
+            foreach (string nomInvertit in arrayNomsInvertits)
+            {
+                Console.WriteLine(nomInvertit);
+            }
+
+            /***************************/
+            /****                   ****/
+            /****    MILESTONE 2    ****/
+            /****                   ****/
+            /***************************/
+
+            Console.WriteLine("\nMILESTONE 2: Notes Alumnes\n");
+
+            double[,] arrayNotes = new double[5, 3]; //5 alumnes, 3 notes
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Introdueix les notes per l'alumne {i + 1}:");
+                //Introdueix 3 notes per teclat
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Nota {j + 1}: ");
+                    //si no convertim a double no les deix afegir, ja que l'array es de doubles
+                    arrayNotes[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            // per a cada un dels alumnes:
+            for (int i = 0; i < 5; i++)
+            {
+                double sumaNotes = 0;
+                //sumem les 3 notes
+                for (int j = 0; j < 3; j++)
+                {
+                    sumaNotes += arrayNotes[i, j];
+                }
+                double mitjana = sumaNotes / 3;
+
+                //Si mitjana es igual o superor a 5: Aprovat, si no suspes
+                string resultat = mitjana >= 5 ? "Aprovat" : "Suspes";
+                Console.WriteLine($"Alumne {i + 1}: Mitjana = {mitjana:F2}, {resultat}");
+            }
+
+            /***************************/
+            /****                   ****/
+            /****    MILESTONE 3    ****/
+            /****                   ****/
+            /***************************/
+
+            Console.WriteLine("\nMILESTONE 3: Sequencia Fibonacci\n");
+
+            Console.Write("Introdueix un numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0, b = 1, c;
+            Console.Write($"Secuencia de Fibonacci hasta {num}: ");
+            Console.Write($"{a}, {b}");
+
+            c = a + b;
+            while (c <= num)
+            {
+                //imprimeixo el valor actual de la sequencia
+                Console.Write(", " + c);
+
+                //actualito valors
+                a = b;
+                b = c;
+                c = a + b;
+            }
         }
-    }
+    } 
 
     class M4
     {
         public static void Execute()
         {
+            /***************************/
+            /****                   ****/
+            /****     MILESTONE     ****/
+            /****     1, 2 i 3      ****/
+            /****                   ****/
+            /***************************/
 
+            Console.WriteLine("MILESTONE 1: Restaurant\n");
+
+            /********* FASE 1 **********/
+            const int MaximPlats = 5;
+            int[] bitllets = { 500, 200, 100, 50, 20, 10, 5 };
+            int preuTotal = 0;
+
+            Dictionary<string, int> menu = new Dictionary<string, int>();
+
+            /********* FASE 2 **********/
+            for (int i = 0; i < MaximPlats; i++)
+            {
+                Console.Write($"Nom del plat {i + 1}: ");
+                string plat = Console.ReadLine();
+
+                Console.Write($"Preu:");
+                int preu = int.Parse(Console.ReadLine());
+                try
+                {
+                    menu.Add(plat, preu);
+                }
+                // error clave repetida
+                catch (ArgumentException ex)
+                {
+                    i--;
+                    Console.WriteLine($"Error: {ex.Message}. Clau ja existeix.");
+                }
+            }
+
+
+            List<string> comanda = new List<string>();
+            bool seguirDemanant = true;
+
+            while (seguirDemanant)
+            {
+                Console.WriteLine("Menú:");
+                foreach (var plat in menu)
+                {
+                    Console.WriteLine($"{plat.Key} | Preu: {plat.Value}e");
+                }
+
+                Console.Write("Introdueix el nom del plat que vols demanar: ");
+                try
+                {
+                    string opcio = Console.ReadLine();
+                    if (menu.ContainsKey(opcio))
+                    {
+                        comanda.Add(opcio);
+                        preuTotal += menu[opcio];
+                    }
+                    else
+                    {
+                        throw new OpcioPlatIncorrectaException("Opció incorrecta");
+                    }
+                }
+                catch (OpcioPlatIncorrectaException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                bool numeroCorrecte = true;
+                //utilito el do while perque mentre l'usuari no premi 1 per seguir escollint o 0 per sortir, torni a repetir la pregunta
+                do
+                {
+                    Console.Write("Vols seguir demanant? (1: Si / 0: No): ");
+                    try
+                    {
+                        int continuar = int.Parse(Console.ReadLine());
+
+                        //si ulusuari prem 0, no vol seguir demanant. Assigno numeroCorrecte a true per a sortir del do while
+                        if (continuar == 0)
+                        {
+                            numeroCorrecte = true;
+                            seguirDemanant = false;
+                        }
+                        // no modifico seguir demanant perque lusuari vol seguir demanant, pero si que assigno numeroCorrecte a true per a que surti del do while
+                        else if (continuar == 1)
+                        {
+                            numeroCorrecte = true;
+                        }
+                        else if (continuar != 1)
+                        {
+                            //lanzo la excepcio
+                            throw new ContinuarDemanantException("Opció incorrecta");
+                        }
+                    }
+                    //assigno numero correcte a fals perque segueixi demanant una opcio valida
+                    catch (FormatException)
+                    {
+                        numeroCorrecte = false;
+                        Console.WriteLine("Opció incorrecta. Introdueix un número.");
+                    }
+                    catch (ContinuarDemanantException ex)
+                    {
+                        numeroCorrecte = false;
+                        Console.WriteLine(ex.Message);
+                    }
+                } while (!numeroCorrecte);
+            }
+
+            /********* FASE 3 **********/
+
+            Console.WriteLine("Comanda:");
+
+            foreach (string plat in comanda)
+            {
+                Console.WriteLine(plat);
+            }
+
+            Console.WriteLine($"Preu total: {preuTotal}e");
+        }
+
+        class OpcioPlatIncorrectaException : Exception
+        {
+            public OpcioPlatIncorrectaException(string message) : base(message) { }
+        }
+        //excepcio que sols mostra un missatge
+        class ContinuarDemanantException : Exception
+        {
+            public ContinuarDemanantException(string message) : base(message) { }
         }
     }
 
@@ -333,7 +600,7 @@ namespace Tarea24
     {
         static void Main(string[] args)
         {
-            M2.Execute();
+            M4.Execute();
         }
     }
 }
